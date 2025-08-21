9b0aab2283df8c48c8f1644694cb3dea9c429eb4
Tariq ali <103600545+Tariqali1308@users.noreply.github.com>
2025-08-22 00:35:00 +0500

    temp comp

--- src/features/main/components/organisms/BannerSection.tsx
+++ src/features/main/components/organisms/BannerSection.tsx
@@ -8,7 +8,7 @@ interface BannerSectionProps {
 const BannerSection: React.FC<BannerSectionProps> = ({ text }) => {
   return (
     <div className="relative h-dvh w-full bg-[linear-gradient(180deg,rgba(0,0,0,0)_58.38%,#000_85.25%),linear-gradient(180deg,#000_6.86%,rgba(0,0,0,0)_54.32%),url('assets/images/banner.png')] dark:bg-[linear-gradient(180deg,rgba(0,0,0,0)_58.38%,#1F2937_85.25%),linear-gradient(180deg,#1F2937_6.86%,rgba(0,0,0,0)_54.32%),url('assets/images/banner.png')] lg:bg-[linear-gradient(180deg,rgba(0,0,0,0)_76.24%,#000_94.69%),linear-gradient(180deg,#000_5.14%,rgba(0,0,0,0)_54.32%),url('assets/images/banner.png')] dark:lg:bg-[linear-gradient(180deg,rgba(0,0,0,0)_76.24%,#1F2937_94.69%),linear-gradient(180deg,#1F2937_5.14%,rgba(0,0,0,0)_54.32%),url('assets/images/banner.png')] bg-cover bg-center">
-      <Text className="absolute bottom-[17%] right-5 lg:top-1/2 lg:right-[100px] text-white dark:text-gray-200 text-lg lg:text-[40px] font-normal font-zen w-[53%] lg:w-[42%]">
+      <Text className="absolute bottom-[17%] right-5 lg:top-1/2 lg:right-[100px] dark:text-white text-gray-200 text-lg lg:text-[40px] font-normal font-zen w-[53%] lg:w-[42%]">
         {text}
       </Text>
     </div>
